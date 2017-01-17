0c3bd287685a52ebb157e789d7318549fd4e7fea
472731 <472731@PC296459.cts.com>
2017-01-17 17:20:09 +0530

    comments added

--- spring-boot-basewebapp/src/main/java/netgloo/controllers/MainController.java
+++ spring-boot-basewebapp/src/main/java/netgloo/controllers/MainController.java
@@ -4,9 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * The Class MainController.
+ */
 @Controller
 public class MainController {
 
+  /**
+   * Index.
+   *
+   * @return the string
+   */
   @RequestMapping("/")
   // @ResponseBody
   public String index() {
